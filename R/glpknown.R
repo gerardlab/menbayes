7b61af43e9e31c56b69960560c606d1d4a40f240
David Gerard <gerard.1787@gmail.com>
2023-04-28 17:17:31 -0400

    export chisq_gl4

--- R/glpknown.R
+++ R/glpknown.R
@@ -258,3 +258,25 @@ marg_f1_dr_pp_glpknown4 <- function(gl,
     return(all)
   }
 }
+
+
+## Chi-Sq for GL
+#' Chi-Sq for GL
+#'
+#' Calculates the MLE genotype and runs a chi-squared test assuming
+#' no double reduction and no preferential pairing.
+#'
+#' @inheritParams marg_f1_dr_pp_glpknown4
+#' @param l1 The first parent's genotype
+#' @param l2 The second parent's genotype.
+#'
+#' @author Mira Thakkar and David Gerard
+chisq_gl4 <- function(gl, l1, l2){
+  ploidy <- 4
+  col_max <- apply(gl, 1, which.max) - 1
+  col_max <- factor(col_max, levels = 0:ploidy)
+  y <- c(table(col_max))
+  output <- chisq_g4(y = y, l1 = l1, l2 = l2)
+
+  return(output)
+}
